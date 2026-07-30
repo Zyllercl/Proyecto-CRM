05dce61f19733a5b01b7966cfb573c3e0776365f
Martin Urisman <mrt.v.urisman@gmail.com>
2026-07-29 21:19:12 -0400

    [ADD] Paleta de colores para consola

---
+++ colors.py
@@ -0,0 +1,14 @@
+# -----------------------------
+# PALETA DE COLORES
+# -----------------------------
+
+class c:
+    # Constantes de color
+    GREEN = "\033[0;32m\033[1m"
+    END = "\033[0m"  # Un solo reset es suficiente
+    RED = "\033[0;31m\033[1m"
+    BLUE = "\033[0;34m\033[1m"
+    YELLOW = "\033[0;33m\033[1m"
+    PURPLE = "\033[0;35m\033[1m"
+    TURQUOISE = "\033[0;36m\033[1m"
+    GRAY = "\033[0;37m\033[1m"
