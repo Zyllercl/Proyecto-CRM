4cfcb7a252c67ef1e265c11ea27297a2159357c8
Martin Urisman <mrt.v.urisman@gmail.com>
2026-08-04 11:05:58 -0400

    [REF] Cambios en la configuracion del menu

--- main.py
+++ main.py
@@ -1,35 +1,35 @@
 from colors import c
+from menu_crud import SubMenu
 
-# Menú Interactivo
 def menu():
     print(f"{c.GRAY}Menú CRM{c.END}")
     print(f"{c.BLUE}1. Ventas")
     print(f"2. Inventario")
     print(f"3. Proveedores")
     print(f"4. Salir{c.END}\n")
-
-# Función principal de la APP
+    
 def app():
     print(f"\n{c.YELLOW}Inicialización Aplicación CRM{c.END}\n")
-
-    menu()
-
     opcion = 0
 
     while opcion != 4:
-        # Opción ingresada por usuario
-        opcion = int(input("Elije una opcion: "))       
-        
-        if opcion == 1:
-            print(f"\n{c.GREEN}[!] Proximamente...{c.END}\n")
-        elif opcion == 2:
-            print(f"\n{c.GREEN}[!] Proximamente...{c.END}\n")
-        elif opcion == 3:
-            print(f"\n{c.GREEN}[!] Proximamente...{c.END}\n")
-        elif opcion == 4:
-            print(f"\n{c.RED}Finalizando programa...{c.END}")
-        else:
-            print(f"\n{c.TURQUOISE}[!] Seleccione una opción válida...{c.END}\n")
+        menu()
+        try:
+            opcion = int(input(f"{c.GRAY}Elije una opcion: {c.END}"))
+            
+            if opcion == 1:
+                print(f"\n{c.GREEN}[!] Proximamente...{c.END}\n")
+            elif opcion == 2:
+                print(f"\n{c.GREEN}[!] Proximamente...{c.END}\n")
+            elif opcion == 3:
+                SubMenu()
+                opcion = 0 # * Se vuelve a mostrar el menú principal
+            elif opcion == 4:
+                print(f"\n{c.RED}Finalizando programa...{c.END}")
+            else:
+                print(f"\n{c.TURQUOISE}[!] Seleccione una opción válida...{c.END}\n")
+        except ValueError:
+            print(f"{c.RED}[!] ERROR: Opción ingresada no es válida !{c.END}")
 
 if __name__ == "__main__":
     app()
