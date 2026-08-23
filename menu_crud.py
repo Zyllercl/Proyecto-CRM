f709ec92350e9d32f5623b2515419d718f83cc43
Martin Urisman <mrt.v.urisman@gmail.com>
2026-08-23 14:55:26 -0400

    [REF] Finalización sub-menú para CRUD

--- menu_crud.py
+++ menu_crud.py
@@ -1,4 +1,5 @@
 from colors import c
+from utils import limpiar_pantalla
 
 class SubMenu():
     def __init__(self):
@@ -8,15 +9,17 @@ class SubMenu():
         opcion = 0
 
         while opcion != 6:
-            print(f"\n{c.BLUE}1. Listar proveedores")
-            print(f"2. Crear proveedor")
-            print(f"3. Buscar proveedor")
-            print(f"4. Actualizar proveedor")
-            print(f"5. Eliminar proveedor")
-            print(f"6. Volver{c.END}\n")
+            print(f"\n{c.GRAY}[SUBMENÚ {gestor.titulo.upper()}]")
+            print(f"{c.BLUE}1. Listar {gestor.titulo}")
+            print(f"2. Crear {gestor.titulo}")
+            print(f"3. Buscar {gestor.titulo}")
+            print(f"4. Actualizar {gestor.titulo}")
+            print(f"5. Eliminar {gestor.titulo}")
+            print(f"6. Volver")
+            print(f"0. Limpiar{c.END}\n")
             
             try:
-                opcion = int(input(f"{c.GRAY}Elije una opcion (submenu): {c.END}"))
+                opcion = int(input(f"{c.GRAY}Elije una opción: {c.END}"))
                 
                 if opcion == 1:
                     gestor.total_lista()
@@ -24,18 +27,26 @@ class SubMenu():
                     try:
                         gestor.crear()
                     except ValueError as e:
-                        print(f"{c.RED}[ERROR] SubMenu: {e}{c.END}")
+                        print(f"{c.RED}[ERROR] Método Crear: {e}{c.END}")
                 elif opcion == 3:
-                    print(f"{c.GREEN}[!] Proximamente...{c.END}")
+                    try:
+                        gestor.buscar()
+                    except ValueError as e:
+                        print(f"{c.RED}[ERROR] Método Buscar: {e}{c.END}")
                 elif opcion == 4:
                     try:
                         gestor.actualizar()
                     except ValueError as e:
-                        print(f"{c.RED}[ERROR] SubMenu: {e}{c.END}")
+                        print(f"{c.RED}[ERROR] Método Actualizar: {e}{c.END}")
                 elif opcion == 5:
-                    print(f"{c.GREEN}[!] Proximamente...{c.END}")
+                    try:
+                        gestor.eliminar()
+                    except ValueError as e:
+                        print(f"{c.RED}[ERROR] Método Eliminar: {e}{c.END}")
                 elif opcion == 6:
                     print(f"{c.YELLOW}Volviendo al menú principal...{c.END}\n")
+                elif opcion == 0:
+                    limpiar_pantalla()
                 else:
                     print(f"{c.TURQUOISE}[!] Seleccione una opción válida...{c.END}")
             except ValueError:
